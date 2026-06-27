4a3e41ab313b1329f89af61bdaf96e78c685e5fc
oli <oli@goodfire.ai>
2026-06-27 21:14:57 +0000

    snapshot p-4d44d7be

--- param_decomp_lab/experiments/lm/launch.py
+++ param_decomp_lab/experiments/lm/launch.py
@@ -67,7 +67,7 @@ _RANK_ENV = r'''export NCCL_DEBUG=WARN
 export MALLOC_ARENA_MAX=2
 export XLA_PYTHON_CLIENT_MEM_FRACTION=0.92
 export XLA_PJRT_GPU_HOST_MEMORY_LIMIT_GB=1024
-export XLA_FLAGS="--xla_gpu_enable_command_buffer="
+export XLA_FLAGS="--xla_gpu_enable_command_buffer= --xla_gpu_autotune_level=0 --xla_dump_to=/mnt/data/artifacts/mechanisms/param-decomp/hlo_hoist_replicate"
 export PD_PROFILE_TRACE=0
 export PD_PROFILE_START=5
 export PD_PROFILE_STEPS=3
